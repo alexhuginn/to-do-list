34d339a4f56951e8451313842e192247b8da4ef6
Oleksandr Yarotskyi <iskander.htf@gmail.com>
2020-04-29 23:01:21 +0300

    add new packages for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,50 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+// File paths
+const files = {
+  scssPath: 'scss/**/*.scss',
+  jsPath: 'js/**/script.js'
 }
 
-exports.default = defaultTask
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass()).on("error", sass.logError)
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('css'))
+}
+
+function jsTask() {
+  return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('js'))
+}
+
+function watchTask() {
+  watch(
+    [files.scssPath, files.jsPath],
+    parallel(scssTask, jsTask)
+  )
+}
+
+exports.default = series(
+  parallel(scssTask, jsTask),
+  watchTask
+)
